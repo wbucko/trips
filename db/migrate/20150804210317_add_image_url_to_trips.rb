9b3d84717be91a6e688bcb97ecc50d47898fd94e
wbucko <bucko.wojciech@gmail.com>
2015-09-03 07:26:41 +0200

    removed migration file

--- db/migrate/20150804210317_add_image_url_to_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddImageUrlToTrips < ActiveRecord::Migration
-  def change
-    add_column :trips, :image_url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901160135) do
+ActiveRecord::Schema.define(version: 20150902222850) do
 
   create_table "trips", force: :cascade do |t|
     t.string   "destination"
