4670f999de65a61c11567613cc56efc510d7d125
wbucko <bucko.wojciech@gmail.com>
2015-09-03 22:48:23 +0200

    changed links little bit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:email])
   	if user and user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to admin_url, notice: "You have successfully logged in"
+  		redirect_to trips_url, notice: "You have successfully logged in"
   	else
   		redirect_to login_url, alert: "Password or email are incorect"
   	end
